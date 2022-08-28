574e8232ae58ba15f716456f7c32163297edbe9b
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2022-08-28 20:01:12 -0300

    feat: create banner test

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render, screen } from 'utils/test-utils'
+
+import Banner from '.'
+
+const props = {
+  imgUrl: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = render(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
