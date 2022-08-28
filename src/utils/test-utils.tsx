24d99890e6e9d9febff1955fcd8dfc452fbc2a91
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2022-08-28 20:46:03 -0300

    feat: create ComicCard test

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import { ReactElement } from 'react'
 import { render, RenderOptions } from '@testing-library/react'
+import { AuthContext, AuthProps } from '../hooks/useAuth'
 
 import { ThemeProvider } from 'styled-components'
 import theme from '../styles/themes/red'
 
-type CustomRenderProps = Omit<RenderOptions, 'queries'>
+const AuthInitialValues = {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  getAuth: () => null,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  validateAuth: () => true,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  createAuth: () => {}
+}
+
+type CustomRenderProps = {
+  cartProviderProps?: AuthProps
+} & Omit<RenderOptions, 'queries'>
 
 const customRender = (
   ui: ReactElement,
-  { ...renderOptions }: CustomRenderProps = {}
-) => render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>, renderOptions)
+  {
+    cartProviderProps = AuthInitialValues,
+    ...renderOptions
+  }: CustomRenderProps = {}
+) =>
+  render(
+    <ThemeProvider theme={theme}>
+      <AuthContext.Provider value={cartProviderProps}>
+        {ui}
+      </AuthContext.Provider>
+    </ThemeProvider>,
+    renderOptions
+  )
 
 export * from '@testing-library/react'
 export { customRender as render }
