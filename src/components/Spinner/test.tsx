a95d3a86399d9fd9cac79b9c0ed560701ae47e6f
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2022-08-28 22:46:01 -0300

    feat: create TextField test

---
+++ src/components/Spinner/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, screen } from 'utils/test-utils'
+import Spinner from '.'
+
+describe('<Spinner />', () => {
+  it('Should render correctly', () => {
+    render(<Spinner />)
+
+    expect(screen.getByTitle(/loading/i)).toBeInTheDocument()
+  })
+})
